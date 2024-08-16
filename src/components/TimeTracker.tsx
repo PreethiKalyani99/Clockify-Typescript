8f54897bd6effba33fba8d2518f2fe9db86b44d8
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-08-16 11:38:33 +0530

    calls Timer component in TimeTracker component

--- src/components/TimeTracker.tsx
+++ src/components/TimeTracker.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { AddTimeEntry } from "./AddTimeEntry";
 import { 
@@ -20,18 +20,27 @@ import { FocusEvent, KeyboardEvent, Constants, SelectedOption } from "../types/t
 import { createTimeEntry, getUserTimeEntries, getProjects, getClients } from "../redux/clockifyThunk";
 import { TimeEntries } from "./TimeEntries";
 import { onStartTimeBlur, onEndTimeBlur, onDurationBlur } from "../utils/onBlurFunctions";
+import { calculateEndTime, convertMilliSecsToHrsMinsSec } from "../utils/hoursAndMinutes";
+import { Timer } from "./Timer";
 
 export function TimeTracker(){
     const { isModalOpen, currentTask, selectedProject, selectedClient, data, projects, clients} = useSelector((state: RootState) => state.clockify)
     const {startTime, endTime, duration, taskName} = currentTask
 
+    const intervalIdRef = useRef<NodeJS.Timeout | null>(null)
+    const timerStart = Date.now()
+
     const timeStart = new Date(startTime)
     const timeEnd = new Date(endTime)
     
     const [showProjects, setShowProjects] = useState(false)
+    const [showActionItems, setShowActionItems] = useState(false)
+
     const [startDateTime, setStartDateTime] = useState(getFormattedTime(timeStart))
     const [endDateTime, setEndDateTime] = useState(getFormattedTime(timeEnd))
     const [totalDuration, setDuration] = useState(duration)
+    const [isTimerOn, setIsTimerOn] = useState(false)
+    const [elapsedTime, setElapsedTime] = useState(0)
 
     const dispatch = useDispatch<AppDispatch>()
 
@@ -40,6 +49,21 @@ export function TimeTracker(){
         dispatch(getProjects())
         dispatch(getClients())
     }, [dispatch])
+
+    useEffect(() => {
+        if(isTimerOn){
+            intervalIdRef.current = setInterval(() => {
+                const currentTimeInMs = Date.now() - timerStart
+                setElapsedTime(currentTimeInMs)
+            }, 1000)
+        }
+        else{
+            clearInterval(intervalIdRef.current as NodeJS.Timeout)
+            intervalIdRef.current = null
+        }
+        return () => clearInterval(intervalIdRef.current as NodeJS.Timeout)
+
+    }, [isTimerOn])
     
     const handleStartTimeBlur = (e: FocusEvent): void => {
         const { isValid, start: newStart, end, duration: newDuration } = onStartTimeBlur(e.target.value, timeEnd)
@@ -112,46 +136,95 @@ export function TimeTracker(){
         }
     }
 
-    const handleSelect = (value: SelectedOption) => {
+    const handleProjectSelect = (value: SelectedOption) => {
         dispatch(updateProjectValue(value))
         setShowProjects(false)
     }
 
     const entriesByWeek = groupEntriesByWeek(data)
 
+    const toggleTimer = () => { 
+        setIsTimerOn(true)
+    }
+
+    const handleTimerStop = () => {
+        const start = new Date()
+        const newEndTime = calculateEndTime(start, convertMilliSecsToHrsMinsSec(elapsedTime))
+        dispatch(createTimeEntry({
+            description: taskName,
+            start: new Date(start).toISOString().split('.')[0] + 'Z',
+            end:  new Date(newEndTime).toISOString().split('.')[0] + 'Z',
+            projectId: selectedProject.value
+        }))
+        setIsTimerOn(false)
+        dispatch(resetState())
+    }
+
+    const toggleActionItem = () => {
+        setShowActionItems(!showActionItems)
+    }
+
+    
+    const handleDiscardTimer = () => {
+        setIsTimerOn(false)
+        setShowActionItems(false)
+        dispatch(resetState())
+    }
+
+    const totalTime = convertMilliSecsToHrsMinsSec(elapsedTime)
     return (
         <>
-            <AddTimeEntry
-                onToggle={toggleProject}
-                selectedProject={selectedProject}
-                selectedClient={selectedClient}
-                timeStart={new Date(startTime)}
-                timeEnd={new Date(endTime)}
-                isModalOpen={isModalOpen}
-                taskDescription={taskName}
-                start={startDateTime}
-                end={endDateTime}
-                totalDuration={totalDuration}
-                days={days}
-                onNameChange={(e) =>  dispatch(updateTaskName(e.target.value))}
-                onStartChange={(e) =>  setStartDateTime(e.target.value)}
-                onEndChange={(e) =>  setEndDateTime(e.target.value)}
-                onDurationChange={(e) =>  setDuration(e.target.value)}
-                onStartBlur={handleStartTimeBlur}
-                onEndBlur={handleEndTimeBlur}
-                onDurationBlur={handleTotalDurationBlur}
-                onDateChange={handleDateChange}
-                onAddTask={addTask}
-                onEnter={handleEnter}
-                setShowProjects={setShowProjects}
-                showProjects={showProjects}
-                onProjectSelect={handleSelect}
-                projects={projects}
-                clients={clients}
-            />
+            {isTimerOn ? 
+                <Timer
+                    totalTime={totalTime}
+                    isModalOpen={isModalOpen}
+                    onNameChange={(e) =>  dispatch(updateTaskName(e.target.value))}
+                    taskName={taskName}
+                    onToggleProject={toggleProject}
+                    selectedProject={selectedProject}
+                    selectedClient={selectedClient}
+                    isTimerOn={isTimerOn}
+                    onTimerStop={handleTimerStop}
+                    ontoggleActionItem={toggleActionItem}
+                    showActionItems={showActionItems}
+                    onDiscard={handleDiscardTimer}
+                    showProjects={showProjects}
+                    setShowProjects={setShowProjects}
+                    onProjectSelect={handleProjectSelect}
+                /> :
+                <AddTimeEntry
+                    onToggle={toggleProject}
+                    selectedProject={selectedProject}
+                    selectedClient={selectedClient}
+                    timeStart={new Date(startTime)}
+                    timeEnd={new Date(endTime)}
+                    isModalOpen={isModalOpen}
+                    taskDescription={taskName}
+                    start={startDateTime}
+                    end={endDateTime}
+                    totalDuration={totalDuration}
+                    days={days}
+                    onNameChange={(e) =>  dispatch(updateTaskName(e.target.value))}
+                    onStartChange={(e) =>  setStartDateTime(e.target.value)}
+                    onEndChange={(e) =>  setEndDateTime(e.target.value)}
+                    onDurationChange={(e) =>  setDuration(e.target.value)}
+                    onStartBlur={handleStartTimeBlur}
+                    onEndBlur={handleEndTimeBlur}
+                    onDurationBlur={handleTotalDurationBlur}
+                    onDateChange={handleDateChange}
+                    onAddTask={addTask}
+                    onEnter={handleEnter}
+                    setShowProjects={setShowProjects}
+                    showProjects={showProjects}
+                    onProjectSelect={handleProjectSelect}
+                    projects={projects}
+                    clients={clients}
+                />
+            }
             <TimeEntries
                 entriesByWeek={entriesByWeek}
                 projects={projects}
+                toggleTimer={toggleTimer}
             />
         </>
     )
